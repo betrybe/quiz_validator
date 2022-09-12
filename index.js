20e807bf1b2d66697f7a0fcc907d20d7fa84756b
Adriel Bento <adrielbb15@gmail.com>
2022-09-12 14:03:36 -0300

    debug

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const validate = require('./validate')
 const core = require('@actions/core');
+const { readdir } = require('fs/promises')
 const root = process.env.GITHUB_WORKSPACE || process.cwd();
 core.notice(root)
-
+readdir(root)
+    .then((data) => core.notice(data))
+    .catch((data) => core.error(data))
+    
 const files = core.getInput('files').split(" ");
 core.notice(files)
 
