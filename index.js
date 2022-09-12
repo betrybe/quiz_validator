15fc98f6007faadc7e7f4a624175fda7cfe27f01
Adriel Bento <adrielbb15@gmail.com>
2022-09-12 13:58:02 -0300

    debug

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const validate = require('./validate')
 const core = require('@actions/core');
+const root = process.env.GITHUB_WORKSPACE || process.cwd();
+core.notice(root)
+
 const files = core.getInput('files').split(" ");
 core.notice(files)
 
