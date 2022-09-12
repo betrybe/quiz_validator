d01f5e0c98dec59f86f6a73979a33df475252def
Adriel Bento <adrielbb15@gmail.com>
2022-09-12 13:54:42 -0300

    debug

--- validate.js
+++ validate.js
@@ -24,7 +24,7 @@ async function validate(files){
 
     const comments = await Promise.all(files.map(async (filename) => {
 
-        const file = await readFile(filename, 'utf8' );
+        const file = await readFile(`/home/runner/work/aceleracao-c-sharp/aceleracao-c-sharp/${filename}`, 'utf8' );
         const result = rules.reduce((acc, rule) => split_and_count_by_separator(file, acc, rule[0], rule[1]), {})
         const checks_result = checks.reduce((acc, check) => {
             const check_name = check[0]
