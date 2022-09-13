29eb81ee11a7bd25128a2c48b5d39f5a5f4ec97d
Adriel Bento <adrielbb15@gmail.com>
2022-09-13 09:33:44 -0300

    debug

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const core = require('@actions/core');
 const { readdir } = require('fs/promises')
 const root = process.env.GITHUB_WORKSPACE || process.cwd();
 core.notice(root)
+core.notice(process.cwd())
 // readdir(root)
 //     .then((data) => core.notice(data))
 //     .catch((data) => core.error(data))
