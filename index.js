b41a506d35b4e50f6ac89f6d9333f91ed13a4e72
Adriel Bento <adrielbb15@gmail.com>
2022-09-13 09:22:21 -0300

    debug

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const core = require('@actions/core');
 const { readdir } = require('fs/promises')
 const root = process.env.GITHUB_WORKSPACE || process.cwd();
 core.notice(root)
-readdir(root)
-    .then((data) => core.notice(data))
-    .catch((data) => core.error(data))
+// readdir(root)
+//     .then((data) => core.notice(data))
+//     .catch((data) => core.error(data))
 
 const files = core.getInput('files').split(" ").shift();
-core.notice(["README.md"])
+core.notice(files)
 
 validate(files)
